aa025215b0a118c91c9bf8a76e8c58fb9382e458
raykolbe <rkolbe@gmail.com>
2013-02-11 09:39:29 -0500

    File went missing from commit. Adding it back in.

---
+++ src/CKEditorModule/Module.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CKEditorModule;
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements
+    ConfigProviderInterface,
+    ViewHelperProviderInterface,
+    AutoloaderProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getViewHelperConfig()
+    {
+        return array(
+            'factories' => array(
+                'ckeditor' => function($sm) {
+                    $config = $sm->getServiceLocator()->get('config');
+                    return new View\Helper\CKEditor(
+                               new Options($config['ckeditor'])
+                           );
+                }
+            )
+        );
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            AutoloaderFactory::STANDARD_AUTOLOADER => array(
+                StandardAutoloader::LOAD_NS => array(
+                    __NAMESPACE__ => __DIR__
+                ),
+            ),
+        );
+    }
+}
